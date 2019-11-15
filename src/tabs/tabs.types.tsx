ad345f808236d8342241247f7d07cae9250bc719
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-11-15 18:25:54 -0500

    reorg

--- src/tabs/tabs.types.tsx
+++ src/tabs/tabs.types.tsx
@@ -4,11 +4,6 @@ import { IReadonlyObservableValue } from 'azure-devops-ui/Core/Observable';
 import { PR } from '../state/types';
 import { IListBoxItem } from 'azure-devops-ui/ListBox';
 
-export type ITab = { filter: Filter; filterItems: FilterItemsDictionary };
-
-export type TabOptions = 'active' | 'draft';
-export type ActiveItemProvider = PR | IReadonlyObservableValue<PR | undefined>;
-
 const searchString = 'searchString';
 const repositories = 'repositories';
 const sourceBranch = 'sourceBranch';
@@ -38,3 +33,6 @@ export const FilterOptions = {
 export type FilterTypes = typeof allFilterOptions[number];
 export type FilterDictionary = Record<FilterTypes, string | string[] | undefined>;
 export type FilterItemsDictionary = Record<Exclude<FilterTypes, typeof searchString>, IListBoxItem[]>;
+export type ITab = { filter: Filter; filterItems: FilterItemsDictionary };
+export type TabOptions = 'active' | 'draft';
+export type ActiveItemProvider = PR | IReadonlyObservableValue<PR | undefined>;
