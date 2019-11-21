cf0a438f47b8daf3bf69e2c090e6b53d15f1d144
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-11-20 22:56:52 -0500

    start of reviewers component

---
+++ src/__tests__/PRTableCellReviewers.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, cleanup } from '@testing-library/react';
+import { IdentityRefWithVote } from 'azure-devops-extension-api/Git/Git';
+import { PRTableCellReviewers } from '../components/PRTableCellReviewers';
+
+afterEach(cleanup);
+
+describe('<PRTableCellReviewers />', () => {
+  const reviewers: IdentityRefWithVote[] = [];
+
+  test('renders successfully column without any reviewers', () => {
+    const { container } = render(<PRTableCellReviewers reviewers={reviewers} />);
+    expect(container).toMatchSnapshot();
+  });
+});
