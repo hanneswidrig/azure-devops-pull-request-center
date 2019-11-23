f86ae5b77a96e2c2c56759830d0fda83a8237e24
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-11-23 18:24:19 -0500

    filters tested

---
+++ src/__tests__/filters.test.ts
@@ -0,0 +1,124 @@
+import '@testing-library/jest-dom/extend-expect';
+import { PR } from '../state/types';
+import {
+  filterByTitle,
+  filterByRepositoryId,
+  filterBySourceBranchDisplayName,
+  filterByTargetBranchDisplayName,
+  filterByCreatedByUserId,
+  filterByReviewers,
+  filterByApprovalStatus,
+  filterByDraftStatus,
+  applyFilter,
+} from '../lib/filters';
+
+describe('Filters for Pull Request Table', () => {
+  test('filterByTitle()', () => {
+    const pullRequest: PR = {
+      title: 'Title',
+    } as PR;
+    expect(filterByTitle(pullRequest, ['Title'])).toBeTruthy();
+    expect(filterByTitle(pullRequest, ['Not'])).toBeFalsy();
+  });
+
+  test('filterByRepositoryId()', () => {
+    const pullRequest: PR = {
+      repositoryId: '1',
+    } as PR;
+    expect(filterByRepositoryId(pullRequest, ['1'])).toBeTruthy();
+    expect(filterByRepositoryId(pullRequest, ['2'])).toBeFalsy();
+  });
+
+  test('filterBySourceBranchDisplayName()', () => {
+    const pullRequest: PR = {
+      sourceBranch: {
+        name: 'Source Branch',
+      },
+    } as PR;
+    expect(filterBySourceBranchDisplayName(pullRequest, ['Source Branch'])).toBeTruthy();
+    expect(filterBySourceBranchDisplayName(pullRequest, ['Target Branch'])).toBeFalsy();
+  });
+
+  test('filterByTargetBranchDisplayName()', () => {
+    const pullRequest: PR = {
+      targetBranch: {
+        name: 'Target Branch',
+      },
+    } as PR;
+    expect(filterByTargetBranchDisplayName(pullRequest, ['Target Branch'])).toBeTruthy();
+    expect(filterByTargetBranchDisplayName(pullRequest, ['Source Branch'])).toBeFalsy();
+  });
+
+  test('filterByCreatedByUserId()', () => {
+    const pullRequest: PR = {
+      createdBy: {
+        id: '1',
+      },
+    } as PR;
+    expect(filterByCreatedByUserId(pullRequest, ['1'])).toBeTruthy();
+    expect(filterByCreatedByUserId(pullRequest, ['2'])).toBeFalsy();
+  });
+
+  test('filterByReviewers()', () => {
+    const pullRequest: PR = {
+      reviewers: [{ id: '1' }],
+    } as PR;
+    expect(filterByReviewers(pullRequest, ['1'])).toBeTruthy();
+    expect(filterByReviewers(pullRequest, ['2'])).toBeFalsy();
+  });
+
+  test('filterByApprovalStatus()', () => {
+    const pullRequest: PR = {
+      myApprovalStatus: '10',
+    } as PR;
+    expect(filterByApprovalStatus(pullRequest, ['10'])).toBeTruthy();
+    expect(filterByApprovalStatus(pullRequest, ['5'])).toBeFalsy();
+  });
+
+  test('filterByDraftStatus()', () => {
+    const pullRequest: PR = {
+      isDraft: true,
+    } as PR;
+    expect(filterByDraftStatus(pullRequest, ['true'])).toBeTruthy();
+    expect(filterByDraftStatus(pullRequest, ['false'])).toBeFalsy();
+  });
+
+  test('applyFilter()', () => {
+    const pullRequests: PR[] = [
+      {
+        title: 'Title',
+        repositoryId: '1',
+        sourceBranch: {
+          name: 'Source Branch',
+        },
+        targetBranch: {
+          name: 'Target Branch',
+        },
+        createdBy: {
+          id: '1',
+        },
+        reviewers: [{ id: '1' }],
+        myApprovalStatus: '10',
+        isDraft: true,
+      },
+      {
+        title: 'Hello World',
+        repositoryId: '1',
+        sourceBranch: {
+          name: 'Source Branch',
+        },
+        targetBranch: {
+          name: 'Target Branch',
+        },
+        createdBy: {
+          id: '1',
+        },
+        reviewers: [{ id: '1' }],
+        myApprovalStatus: '10',
+        isDraft: false,
+      },
+    ] as PR[];
+    const filteredPullRequests = applyFilter(pullRequests, { searchString: 'Hello World' }, 'active');
+    expect(filteredPullRequests.length).toBe(1);
+  });
+});
