276bc3d70ad6e62e7ea76a712b5a2f7784f34e9b
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-11-24 23:09:40 -0500

    add reducer types

--- src/lib/typings.ts
+++ src/lib/typings.ts
@@ -1,1 +1,7 @@
+import { FetchAction } from '../state/actions';
+import { ActionTypes, PrHubState } from '../state/types';
+
 export type Enum<T extends object> = T[keyof T];
+
+type ReducerAction = [Enum<typeof ActionTypes>, () => PrHubState];
+export type SplitReducer = (state: PrHubState, action: FetchAction) => ReducerAction[];
