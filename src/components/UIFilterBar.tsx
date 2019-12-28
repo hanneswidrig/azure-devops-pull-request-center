158569be646c6f83638da7655dc45a55c14fbcac
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-12-27 19:07:45 -0600

    select correct options

--- src/components/UIFilterBar.tsx
+++ src/components/UIFilterBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { FilterBar } from 'azure-devops-ui/FilterBar';
+import { IListBoxItem } from 'azure-devops-ui/ListBox';
 import { DropdownFilterBarItem } from 'azure-devops-ui/Dropdown';
 import { KeywordFilterBarItem } from 'azure-devops-ui/TextFilterBarItem';
 import { DropdownMultiSelection } from 'azure-devops-ui/Utilities/DropdownSelection';
@@ -8,7 +9,7 @@ import { DropdownMultiSelection } from 'azure-devops-ui/Utilities/DropdownSelect
 import { filter } from '..';
 import { ActionTypes } from '../state/types';
 import { ApprovalStatusItem } from './ApprovalStatusItem';
-import { FilterOptions, FilterItemsDictionary } from '../tabs/TabTypes';
+import { FilterOptions, FilterItemsDictionary, FilterDictionaryNonNullable } from '../tabs/TabTypes';
 
 const _repositories = new DropdownMultiSelection();
 const _sourceBranch = new DropdownMultiSelection();
@@ -22,18 +23,17 @@ export const UIFilterBar: React.FC<Props> = ({ filterItems }: Props) => {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    if (filterItems.repositories.length > 0) {
-    }
-    if (filterItems.sourceBranch.length > 0) {
-    }
-    if (filterItems.targetBranch.length > 0) {
-    }
-    if (filterItems.author.length > 0) {
-    }
-    if (filterItems.reviewer.length > 0) {
-    }
-    if (filterItems.myApprovalStatus.length > 0) {
-    }
+    const defaultValues: FilterDictionaryNonNullable = {
+      searchString: filter.getFilterItemValue<string>(FilterOptions.searchString) ?? '',
+      repositories: filter.getFilterItemValue<string[]>(FilterOptions.repositories) ?? [],
+      sourceBranch: filter.getFilterItemValue<string[]>(FilterOptions.sourceBranch) ?? [],
+      targetBranch: filter.getFilterItemValue<string[]>(FilterOptions.targetBranch) ?? [],
+      author: filter.getFilterItemValue<string[]>(FilterOptions.author) ?? [],
+      reviewer: filter.getFilterItemValue<string[]>(FilterOptions.reviewer) ?? [],
+      myApprovalStatus: filter.getFilterItemValue<string[]>(FilterOptions.myApprovalStatus) ?? [],
+    };
+    const matchedFilterValues = matchingFilterValues(defaultValues, filterItems);
+    selectExistingDefaultFilterValue(matchedFilterValues, filterItems);
   }, [filterItems]);
 
   return (
@@ -95,5 +95,57 @@ export const UIFilterBar: React.FC<Props> = ({ filterItems }: Props) => {
         />
       </FilterBar>
     </div>
+  );
+};
+
+type ExtractCommonValues = (
+  listBoxItem: IListBoxItem<{}>[],
+  defaultValuesItem: string | string[],
+) => IListBoxItem<{}>[];
+
+type MatchingFilterValues = (
+  defaultValues: FilterDictionaryNonNullable,
+  filterItems: FilterItemsDictionary,
+) => FilterItemsDictionary;
+
+type SelectExistingDefaultFilterValue = (
+  matchedFilterValues: FilterItemsDictionary,
+  allFilterItems: FilterItemsDictionary,
+) => void;
+
+const extractCommonValues: ExtractCommonValues = (listBoxItem, defaultValuesItem) => {
+  const matchingValues = listBoxItem.filter(item => defaultValuesItem.includes(item.id));
+  return listBoxItem.length > 0 ? matchingValues : [];
+};
+
+const matchingFilterValues: MatchingFilterValues = (defaultValues, filterItems) => {
+  return {
+    repositories: extractCommonValues(filterItems.repositories, defaultValues.repositories),
+    sourceBranch: extractCommonValues(filterItems.sourceBranch, defaultValues.sourceBranch),
+    targetBranch: extractCommonValues(filterItems.targetBranch, defaultValues.targetBranch),
+    author: extractCommonValues(filterItems.author, defaultValues.author),
+    reviewer: extractCommonValues(filterItems.reviewer, defaultValues.reviewer),
+    myApprovalStatus: extractCommonValues(filterItems.myApprovalStatus, defaultValues.myApprovalStatus),
+  };
+};
+
+const selectExistingDefaultFilterValue: SelectExistingDefaultFilterValue = (matchedFilterValues, allFilterItems) => {
+  matchedFilterValues.repositories.forEach(item =>
+    _repositories.select(allFilterItems.repositories.findIndex(filterItem => filterItem.id === item.id)),
+  );
+  matchedFilterValues.sourceBranch.forEach(item =>
+    _sourceBranch.select(allFilterItems.sourceBranch.findIndex(filterItem => filterItem.id === item.id)),
+  );
+  matchedFilterValues.targetBranch.forEach(item =>
+    _targetBranch.select(allFilterItems.targetBranch.findIndex(filterItem => filterItem.id === item.id)),
+  );
+  matchedFilterValues.author.forEach(item =>
+    _author.select(allFilterItems.author.findIndex(filterItem => filterItem.id === item.id)),
+  );
+  matchedFilterValues.reviewer.forEach(item =>
+    _reviewer.select(allFilterItems.reviewer.findIndex(filterItem => filterItem.id === item.id)),
+  );
+  matchedFilterValues.myApprovalStatus.forEach(item =>
+    _myApprovalStatus.select(allFilterItems.myApprovalStatus.findIndex(filterItem => filterItem.id === item.id)),
   );
 };
--- src/tabs/TabTypes.ts
+++ src/tabs/TabTypes.ts
@@ -31,6 +31,7 @@ export const FilterOptions = {
 } as const;
 export type FilterTypes = typeof allFilterOptions[number];
 export type FilterDictionary = Record<FilterTypes, string | string[] | undefined>;
+export type FilterDictionaryNonNullable = Record<FilterTypes, string | string[]>;
 export type FilterItemsDictionary = Record<Exclude<FilterTypes, typeof searchString>, IListBoxItem[]>;
 export type ITab = { filterItems: FilterItemsDictionary; store: PrHubState };
 export type ActiveItemProvider = PR | IReadonlyObservableValue<PR | undefined>;
