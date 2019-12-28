c8e1e107ade5b7f5d5aaaab98d87f368ced6560a
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-12-27 19:25:28 -0600

    fix is full screen mode not being reflected properly

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -24,12 +24,119 @@ const defaultSettingValues: DefaultSettings = {
   filterValues: undefined,
 };
 
+const isFullScreenModeItems: IChoiceGroupOption[] = [
+  { key: 'false', text: 'Disabled', iconProps: { iconName: 'SidePanel', title: 'Show Azure DevOps UI Shell' } },
+  { key: 'true', text: 'Enabled', iconProps: { iconName: 'ScaleUp', title: 'Hide Azure DevOps UI Shell' } },
+];
+
+const selectedTabItems: IChoiceGroupOption[] = [
+  { key: 'active', text: 'Active' },
+  { key: 'draft', text: 'Draft' },
+  { key: 'recentlyCompleted', text: 'Completed (10 Most Recent)' },
+];
+
+const sortDirectionItems: IChoiceGroupOption[] = [
+  { key: 'desc', text: 'Newest First', iconProps: { iconName: 'SortDown' } },
+  { key: 'asc', text: 'Oldest First', iconProps: { iconName: 'SortUp' } },
+];
+
+type ChoiceGroupChanged = (
+  selectedOption: IChoiceGroupOption | undefined,
+  setSettingValues: React.Dispatch<React.SetStateAction<DefaultSettings>>,
+  dispatch?: Dispatch<any>,
+) => void;
+
+type CompoundButtonChanged = (
+  decision: 'save' | 'clear',
+  setSettingValues: React.Dispatch<React.SetStateAction<DefaultSettings>>,
+) => void;
+
+type ToggleChanged = (
+  selectedOption: boolean | undefined,
+  setSettingValues: React.Dispatch<React.SetStateAction<DefaultSettings>>,
+) => void;
+
+type ResetChanges = (
+  setSettingValues: React.Dispatch<React.SetStateAction<DefaultSettings>>,
+  dispatch: Dispatch<any>,
+) => void;
+
+type ApplyChanges = (defaultSettings: DefaultSettings, dispatch: Dispatch<any>) => void;
+
+const isFullScreenModeChanged: ChoiceGroupChanged = (selectedOption, setSettingValues, dispatch) => {
+  const isFullScreenMode = selectedOption?.key === 'true' ?? false;
+  setSettingValues(values => ({ ...values, isFullScreenMode: isFullScreenMode }));
+  if (dispatch) {
+    dispatch(setFullScreenMode(isFullScreenMode));
+  }
+};
+
+const isSavingFilterItemsChanged: CompoundButtonChanged = (decision, setSettingValues) => {
+  setSettingValues(values => ({ ...values, isSavingFilterItems: decision === 'save' }));
+};
+
+const isFilterVisibleChanged: ToggleChanged = (selectedOption, setSettingValues) => {
+  setSettingValues((values: DefaultSettings) => ({ ...values, isFilterVisible: selectedOption ?? false }));
+};
+
+const selectedTabChanged: ChoiceGroupChanged = (selectedOption, setSettingValues) => {
+  const option = selectedTabItems.find(option => option.key === selectedOption?.key) ?? selectedTabItems[0];
+  setSettingValues(values => ({ ...values, selectedTab: option.key as TabOptions }));
+};
+
+const sortDirectionChanged: ChoiceGroupChanged = (selectedOption, setSettingValues) => {
+  const option = sortDirectionItems.find(option => option.key === selectedOption?.key) ?? sortDirectionItems[0];
+  setSettingValues(values => ({ ...values, sortDirection: option.key as SortDirection }));
+};
+
+const resetChanges: ResetChanges = (setSettingValues, dispatch) => {
+  setSettingValues(defaultSettingValues);
+  dispatch(setFullScreenMode(defaultSettingValues.isFullScreenMode));
+};
+
+const applyChanges: ApplyChanges = (defaultSettings, dispatch) => {
+  dispatch(setFilterBar(defaultSettings.isFilterVisible));
+  dispatch(setSelectedTab(defaultSettings.selectedTab));
+  dispatch(setSortDirection(defaultSettings.sortDirection));
+  dispatch(saveSettings(defaultSettings));
+  dispatch(toggleSettingsPanel());
+};
+
+const defaultSettingsEquality = (left: DefaultSettings, right: DefaultSettings): boolean => {
+  const isFilterVisibleNotEqual =
+    (left.isFilterVisible ?? defaultSettingValues.isFilterVisible) !==
+    (right.isFilterVisible ?? defaultSettingValues.isFilterVisible);
+
+  const isFullScreenModeNotEqual =
+    (left.isFullScreenMode ?? defaultSettingValues.isFullScreenMode) !==
+    (right.isFullScreenMode ?? defaultSettingValues.isFullScreenMode);
+
+  const isSavingFilterItemsNotEqual =
+    (left.isSavingFilterItems ?? defaultSettingValues.isSavingFilterItems) !==
+    (right.isSavingFilterItems ?? defaultSettingValues.isSavingFilterItems);
+
+  const selectedTabNotEqual =
+    (left.selectedTab ?? defaultSettingValues.selectedTab) !== (right.selectedTab ?? defaultSettingValues.selectedTab);
+
+  const sortDirectionNotEqual =
+    (left.sortDirection ?? defaultSettingValues.sortDirection) !==
+    (right.sortDirection ?? defaultSettingValues.sortDirection);
+
+  return (
+    isFilterVisibleNotEqual ||
+    isFullScreenModeNotEqual ||
+    isSavingFilterItemsNotEqual ||
+    selectedTabNotEqual ||
+    sortDirectionNotEqual
+  );
+};
+
 export const SettingsPanel: React.FC = () => {
   const store = useSelector((store: PrHubState) => store);
   const dispatch = useDispatch();
   const [settingValues, setSettingValues] = React.useState<DefaultSettings>({
     isFilterVisible: store.settings.defaults.isFilterVisible,
-    isFullScreenMode: store.settings.defaults.isFullScreenMode,
+    isFullScreenMode: store.settings.defaults.isFullScreenMode || store.ui.isFullScreenMode,
     selectedTab: store.settings.defaults.selectedTab,
     sortDirection: store.settings.defaults.sortDirection,
     isSavingFilterItems: store.settings.defaults.isSavingFilterItems,
@@ -106,110 +213,5 @@ export const SettingsPanel: React.FC = () => {
         />
       </Stack>
     </Panel>
-  );
-};
-
-const isFullScreenModeItems: IChoiceGroupOption[] = [
-  { key: 'false', text: 'Disabled', iconProps: { iconName: 'SidePanel', title: 'Show Azure DevOps UI Shell' } },
-  { key: 'true', text: 'Enabled', iconProps: { iconName: 'ScaleUp', title: 'Hide Azure DevOps UI Shell' } },
-];
-
-const selectedTabItems: IChoiceGroupOption[] = [
-  { key: 'active', text: 'Active' },
-  { key: 'draft', text: 'Draft' },
-  { key: 'recentlyCompleted', text: 'Completed (10 Most Recent)' },
-];
-
-const sortDirectionItems: IChoiceGroupOption[] = [
-  { key: 'desc', text: 'Newest First', iconProps: { iconName: 'SortDown' } },
-  { key: 'asc', text: 'Oldest First', iconProps: { iconName: 'SortUp' } },
-];
-
-type ChoiceGroupChanged = (
-  selectedOption: IChoiceGroupOption | undefined,
-  setSettingValues: React.Dispatch<React.SetStateAction<DefaultSettings>>,
-  dispatch?: Dispatch<any>,
-) => void;
-
-type CompoundButtonChanged = (
-  decision: 'save' | 'clear',
-  setSettingValues: React.Dispatch<React.SetStateAction<DefaultSettings>>,
-) => void;
-
-type ToggleChanged = (
-  selectedOption: boolean | undefined,
-  setSettingValues: React.Dispatch<React.SetStateAction<DefaultSettings>>,
-) => void;
-
-const isFullScreenModeChanged: ChoiceGroupChanged = (selectedOption, setSettingValues, dispatch) => {
-  const isFullScreenMode = selectedOption?.key === 'true' ?? false;
-  setSettingValues(values => ({ ...values, isFullScreenMode: isFullScreenMode }));
-  if (dispatch) {
-    dispatch(setFullScreenMode(isFullScreenMode));
-  }
-};
-
-const isSavingFilterItemsChanged: CompoundButtonChanged = (decision, setSettingValues) => {
-  setSettingValues(values => ({ ...values, isSavingFilterItems: decision === 'save' }));
-};
-
-const isFilterVisibleChanged: ToggleChanged = (selectedOption, setSettingValues) => {
-  setSettingValues((values: DefaultSettings) => ({ ...values, isFilterVisible: selectedOption ?? false }));
-};
-
-const selectedTabChanged: ChoiceGroupChanged = (selectedOption, setSettingValues) => {
-  const option = selectedTabItems.find(option => option.key === selectedOption?.key) ?? selectedTabItems[0];
-  setSettingValues(values => ({ ...values, selectedTab: option.key as TabOptions }));
-};
-
-const sortDirectionChanged: ChoiceGroupChanged = (selectedOption, setSettingValues) => {
-  const option = sortDirectionItems.find(option => option.key === selectedOption?.key) ?? sortDirectionItems[0];
-  setSettingValues(values => ({ ...values, sortDirection: option.key as SortDirection }));
-};
-
-type ResetChanges = (
-  setSettingValues: React.Dispatch<React.SetStateAction<DefaultSettings>>,
-  dispatch: Dispatch<any>,
-) => void;
-const resetChanges: ResetChanges = (setSettingValues, dispatch) => {
-  setSettingValues(defaultSettingValues);
-  dispatch(setFullScreenMode(defaultSettingValues.isFullScreenMode));
-};
-
-type ApplyChanges = (defaultSettings: DefaultSettings, dispatch: Dispatch<any>) => void;
-const applyChanges: ApplyChanges = (defaultSettings, dispatch) => {
-  dispatch(setFilterBar(defaultSettings.isFilterVisible));
-  dispatch(setSelectedTab(defaultSettings.selectedTab));
-  dispatch(setSortDirection(defaultSettings.sortDirection));
-  dispatch(saveSettings(defaultSettings));
-  dispatch(toggleSettingsPanel());
-};
-
-const defaultSettingsEquality = (left: DefaultSettings, right: DefaultSettings): boolean => {
-  const isFilterVisibleNotEqual =
-    (left.isFilterVisible ?? defaultSettingValues.isFilterVisible) !==
-    (right.isFilterVisible ?? defaultSettingValues.isFilterVisible);
-
-  const isFullScreenModeNotEqual =
-    (left.isFullScreenMode ?? defaultSettingValues.isFullScreenMode) !==
-    (right.isFullScreenMode ?? defaultSettingValues.isFullScreenMode);
-
-  const isSavingFilterItemsNotEqual =
-    (left.isSavingFilterItems ?? defaultSettingValues.isSavingFilterItems) !==
-    (right.isSavingFilterItems ?? defaultSettingValues.isSavingFilterItems);
-
-  const selectedTabNotEqual =
-    (left.selectedTab ?? defaultSettingValues.selectedTab) !== (right.selectedTab ?? defaultSettingValues.selectedTab);
-
-  const sortDirectionNotEqual =
-    (left.sortDirection ?? defaultSettingValues.sortDirection) !==
-    (right.sortDirection ?? defaultSettingValues.sortDirection);
-
-  return (
-    isFilterVisibleNotEqual ||
-    isFullScreenModeNotEqual ||
-    isSavingFilterItemsNotEqual ||
-    selectedTabNotEqual ||
-    sortDirectionNotEqual
   );
 };
