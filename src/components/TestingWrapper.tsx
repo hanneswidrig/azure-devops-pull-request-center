3d95b24a785ee7c6a83c925729835f14625634a0
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-12-31 17:43:35 -0600

    testing useRefreshTicker

--- src/components/TestingWrapper.tsx
+++ src/components/TestingWrapper.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import { Store, AnyAction } from 'redux';
 
-type TestingWrapperType = { children?: React.ReactNode; store: Store<any, AnyAction> };
+export type WrapperType = { children?: React.ReactNode };
+
+type TestingWrapperType = WrapperType & { store: Store<any, AnyAction> };
 export const TestingWrapper: React.FC<TestingWrapperType> = ({ children, store }: TestingWrapperType) => {
   return <Provider store={store}>{children}</Provider>;
 };
